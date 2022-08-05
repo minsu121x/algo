dcf4baede1db8f4a57672e134c9aaac7f22a9783
SSAFY <SSAFY@DESKTOP>
2022-08-05 17:55:36 +0900

    [2022-08-05 조민수] 임시저장

---
+++ src/조민수/P017_BJ2493_탑.java
@@ -0,0 +1,27 @@
+package 조민수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class P017_BJ2493_탑 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int dnaLen=0;
+		int codeLen=0;
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		String st=br.readLine();
+		String[] str=st.split(" ");
+		dnaLen=Integer.parseInt(str[0]);
+		codeLen=Integer.parseInt(str[1]);
+		Queue<Integer> codeCheck = new LinkedList<Integer>(); 
+		
+	}
+
+}
