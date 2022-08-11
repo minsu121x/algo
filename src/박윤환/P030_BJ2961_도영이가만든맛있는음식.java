eb42e03148a5c481d2efdd3ac1bdffba939e9674
ParkYunHwan <dope2514@gmail.com>
2022-08-11 17:42:45 +0900

    [2022-08-11 박윤환] P030, P031 풀이 완료

--- src/박윤환/P030_BJ2961_도영이가만든맛있는음식.java
+++ src/박윤환/P030_BJ2961_도영이가만든맛있는음식.java
@@ -1,10 +1,57 @@
 package 박윤환;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P030_BJ2961_도영이가만든맛있는음식 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static int N, result;
+	static int[][] sourBitter;
+	static boolean[] isSelected;
+	
+	// 가능한 재료 조합 구하기
+	static void subset(int index) {
+		// 재료를 골랐다면
+		if(index == N) {
+			int sourSum = 1;
+			int bitterSum = 0;
+			boolean forFlag = false;	// 원소개수가 1개일 경우 계산하지 않기위해 넣는 flag 변수
+			for(int i=0; i<N; i++) {	// 재료 개수만큼 반복
+				if(isSelected[i]) {	// 선택된 재료일 경우
+					forFlag = true;	// flag에 체크하고
+					sourSum *= sourBitter[i][0];	// 신맛을 구하고
+					bitterSum += sourBitter[i][1];	// 쓴맛을 구한다
+				}
+			}
+			if(forFlag && Math.abs(sourSum-bitterSum) < result) {
+				result = Math.abs(sourSum-bitterSum);	// 신맛과 쓴맛의 차이의 최소값 저장
+			}
+			
+			return;
+ 		}
+		
+		isSelected[index] = true;
+		subset(index+1);
+		isSelected[index] = false;
+		subset(index+1);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());	// 재료의 개수
+		sourBitter = new int[N][2];	// 재료를 저장할 배열
+		for(int i=0; i<N; i++) {
+			String[] st = br.readLine().split(" ");
+			sourBitter[i][0] = Integer.parseInt(st[0]);
+			sourBitter[i][1] = Integer.parseInt(st[1]);
+		}
+		
+		result = 1_000_000_000;		// 최소값을 저장할 변수
+		isSelected = new boolean[N];
+		subset(0);
+		
+		System.out.println(result);
 	}
 
 }
--- src/박윤환/P031_BJ3040_백설공주와일곱난쟁이.java
+++ src/박윤환/P031_BJ3040_백설공주와일곱난쟁이.java
@@ -1,10 +1,55 @@
 package 박윤환;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class P031_BJ3040_백설공주와일곱난쟁이 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static final int N = 9;		// 모자 개수
+	static final int R = 7;		// 난쟁이 수
+	static int[] hat, dwarf;
+	static StringBuilder sb;
+	
+	// 난쟁이가 쓸 모자를 고르는 메소드
+	static void comb(int cnt, int start) {
+		// 모자를 난쟁이 수만큼 골랐다면
+		if(cnt == R) {
+			int sum = 0;
+			for (int d : dwarf) {	// 모자 번호의 합을 구한다
+				sum += d;
+			}
+			if(sum == 100) {	// 합이 100이면
+				for (int d : dwarf) {
+					sb.append(d).append("\n");	// 결과 출력
+				}
+			}
+			return;
+		}
+		
+		// 가능한 모든 수에 대해 시도
+		for(int i=start; i<N; i++) {
+			// 앞쪽에서 선택되지 않았다면 수를 사용
+			dwarf[cnt] = hat[i];
+			// 다음 수 뽑으러 가기
+			comb(cnt + 1, i+1);
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		
+		hat = new int[N];	// 모자 배열
+		dwarf = new int[R];	// 난쟁이 배열
+		
+		for(int i=0; i<N; i++) {
+			hat[i] = Integer.parseInt(br.readLine());
+		}
+		
+		comb(0, 0);
+		
+		System.out.println(sb);
+		
 	}
 
 }
