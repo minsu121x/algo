ad4b4d0d1a77bc9bd4698f2495de2bc9b0b16e14
ParkYunHwan <dope2514@gmail.com>
2022-08-12 22:02:39 +0900

    [2022-08-12 박윤환] P034 코드 최적화

--- src/박윤환/P034_SWEA4012_요리사.java
+++ src/박윤환/P034_SWEA4012_요리사.java
@@ -11,62 +11,36 @@ public class P034_SWEA4012_요리사 {
 	static int N, min;
 	static int[][] syn;
 	static boolean[] food;
-	static int[] foodA, foodB, ingrA, ingrB;
-	static ArrayList<Integer> listA, listB;
-	
-	// A와 B에서 사용할 식재료들의 조합을 구하는 메소드
-	static void combIngr(int[] f, int[] ingr, ArrayList<Integer> list, int cnt, int start) {
-		// 2개 선택 됐다면
-		if(cnt == 2) {
-			int sum = 0;
-			sum = syn[ingr[0]][ingr[1]] + syn[ingr[1]][ingr[0]];	// 시너지 합을 구한다
-			list.add(sum);	// 리스트에 전력을 저장한다
-			return;
-		}
-		// 가능한 모든 조합 선택
-		for(int i=start; i<N/2; i++) {
-			ingr[cnt] = f[i];
-			combIngr(f, ingr, list, cnt+1, i+1);
-		}
-		
-	}
 	
 	// 재료를 음식 A와 B로 나누는 조합을 구하는 메소드
 	static void combFood(int index, int cnt) {
 		// 모든 재료의 선택이 끝났으면
 		if(index == N) {
-			if(cnt != N/2) return;	// 절반으로 나눠지지 않았다면 다시 선택
-			int idxA = 0;
-			int idxB = 0;
-			for(int i=0; i<N; i++) {	// 선택한 재료를 저장
-				if(food[i]) {
-					foodA[idxA++] = i;
-				} else {
-					foodB[idxB++] = i;
+			if(cnt != N/2) return;	// 절반으로 안나뉘었으면 다시 선
+			int sumA = 0;
+			int sumB = 0;
+			for(int i=0; i<N; i++) {	// 처음부터 탐색
+				for(int j=i+1; j<N; j++) {	// 재료 하나를 고르고 그 뒤부터 탐색
+					// true이면 음식A에, false면 음식B에 사용
+					if(food[i] && food[j]) {	// i와 j가 음식A에 선택됐다면
+						sumA += syn[i][j] + syn[j][i];	// A의 시너지 합 계산
+					} else if(!food[i] && !food[j]) {	// i와 j가 음식B에 선택됐다면
+						sumB += syn[i][j] + syn[j][i];	// B와 시너지 합 계산
+					}
 				}
 			}
-			listA = new ArrayList<>();	// A음식의 전력을 저장할 배열
-			listB = new ArrayList<>();	// B음식의 전력을 저장할 배열
-			
-			// 각 음식들에 선택된 식재료들 중 2개를 선택하는 조합
-			combIngr(foodA, ingrA, listA, 0, 0);
-			combIngr(foodB, ingrB, listB, 0, 0);
-			
-			int sum = 0;
-			for(int i=0; i<listA.size(); i++) {
-				sum += listA.get(i);
-				sum -= listB.get(i);
-			}
-			min = Math.min(min, Math.abs(sum));
+			// 계산한 시너지 합의 차를 계산하고 최솟값인지 판별 후 저장
+			min = Math.min(Math.abs(sumA - sumB), min);
 			
 			return;
 		}
+		
 		// 재료 선택
 		food[index] = true;
-		combFood(index+1, ++cnt);
+		combFood(index+1, cnt+1);
 		// 재료 미선택
 		food[index] = false;
-		combFood(index+1, --cnt);
+		combFood(index+1, cnt);
 	
 	}
 	
@@ -87,11 +61,7 @@ public class P034_SWEA4012_요리사 {
 				}
 			}
 			
-			food = new boolean[N];
-			foodA = new int[N/2];
-			foodB = new int[N/2];
-			ingrA = new int[2];
-			ingrB = new int[2];
+			food = new boolean[N];	// 어떤 음식에 어떤 재료를 쓸지 기록하는 배열
 			min = Integer.MAX_VALUE;
 			combFood(0, 0);
 			
