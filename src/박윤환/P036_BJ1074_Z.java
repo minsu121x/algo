40b44e3fe0ef1c5cacebfa88fa4a6cffd040b9a5
ParkYunHwan <dope2514@gmail.com>
2022-08-16 21:15:38 +0900

    [2022-08-16 박윤환] P036, P037 풀이 완료

--- src/박윤환/P036_BJ1074_Z.java
+++ src/박윤환/P036_BJ1074_Z.java
@@ -4,18 +4,42 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class P036_BJ1074_Z {
+
+    static int cnt;
+
+    static void search(int size, int r, int c) {
+        if(size == 1) {
+            return;
+        }
+        // r, c의 위치가 어느 분면이냐에 따라
+        if(r < size/2 && c < size/2) {  // 1사분면
+            search(size/2, r, c);
+        } else if(r < size/2 && c >= size/2) {  // 2사분면
+            cnt += size*size/4;     // 2사분면의 번호 시작은 현재 크기의 절반의 제곱
+            search(size/2, r, c-size/2);    // 시작번호만큼 카운트를 더하고 사분면내에 새로운 탐색
+        } else if(r >= size/2 && c < size/2) {  // 3사분면
+            cnt += size*size/4 * 2; // 3사분면은 2사분면 시작의 2배
+            search(size/2, r-size/2, c);
+        } else if(r >= size/2 && c >= size/2) { // 4사분면
+            cnt += size*size/4 * 3; // 4사분면은 2사분면 시작의 3배
+            search(size/2, r-size/2, c-size/2);
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] chem = new int[N][2];
+        String[] st = br.readLine().split(" ");
 
-        for(int i=0; i<N; i++) {
-            String[] st = br.readLine().split(" ");
-            chem[i][0] = Integer.parseInt(st[0]);
-            chem[i][1] = Integer.parseInt(st[1]);
-        }
+        int N = Integer.parseInt(st[0]);
+        int r = Integer.parseInt(st[1]);
+        int c = Integer.parseInt(st[2]);
 
+        int size = (int) Math.pow(2, N);    // 배열의 크기는 2^N
 
+        search(size, r, c);
+
+        System.out.println(cnt);
     }
 }
--- src/박윤환/P037_JO1828_냉장고.java
+++ src/박윤환/P037_JO1828_냉장고.java
@@ -1,4 +1,36 @@
 package 박윤환;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class P037_JO1828_냉장고 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());    // 화학물질 수
+        int[][] chem = new int[N][2];       // 화학물질을 저장할 배열
+        ArrayList<int[]> list = new ArrayList<>();  // 냉장고 개수에 넣을 기준이 되는 화학물질을 넣을 list
+
+        for(int i=0; i<N; i++) {    // 화학물질을 배열에 저장
+            String[] st = br.readLine().split(" ");
+            chem[i][0] = Integer.parseInt(st[0]);
+            chem[i][1] = Integer.parseInt(st[1]);
+        }
+
+        Arrays.sort(chem, (o1, o2) -> {
+            return o1[1] != o2[1] ? o1[1] - o2[1] : o1[0] - o2[0];
+        });   // 화학물질의 최고온도를 기준으로 오름차순 정렬, 만약 같으면 최저온도 기준으로 오름차순 정렬
+
+        list.add(chem[0]);  // 가장 최고온도가 낮은 화학물질을 list에 넣는다
+
+        for(int i=1, size=chem.length; i<size; i++) {   // 남은 화학물질 만큼 반복
+            if(list.get(list.size()-1)[1] < chem[i][0]) {   // 다음 범위의 화학물질이 list의 가장 마지막 화학물질과 겹치지 않으면
+                list.add(chem[i]);  // list에 추가
+            }
+        }
+
+        System.out.println(list.size());    // list의 크기가 냉장고의 개수
+    }
 }
