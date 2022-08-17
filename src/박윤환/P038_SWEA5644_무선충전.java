57a78687bafbadf9f5fe4dd162a64e638ac8ec5a
ParkYunHwan <dope2514@gmail.com>
2022-08-17 23:07:56 +0900

    [2022-08-17 박윤환] P040 풀이완료, P038 주석 추가

--- src/박윤환/P038_SWEA5644_무선충전.java
+++ src/박윤환/P038_SWEA5644_무선충전.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 
 public class P038_SWEA5644_무선충전 {
 
+    // int[]타입의 객체를 리스트 내에 있는지 판별하는 메소드
     static boolean contains(ArrayList<int[]> list, int[] array) {
         return list.stream()
                 .anyMatch(x -> Arrays.equals(x, array));
@@ -18,21 +19,23 @@ public class P038_SWEA5644_무선충전 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
-        for(int tc=1; tc<=T; tc++) {
+        int T = Integer.parseInt(br.readLine());    // 테스트케이스 개수
+        for(int tc=1; tc<=T; tc++) {    // 테스트케이스 반복
             String[] st = br.readLine().split(" ");
-            int M = Integer.parseInt(st[0]);
-            int A = Integer.parseInt(st[1]);
+            int M = Integer.parseInt(st[0]);    // 이동 시간
+            int A = Integer.parseInt(st[1]);    // BC의 개수
             int result = 0;
-            int[][] info = new int[2][M];
-            int[][] BC = new int[A][4];
-            int[][] user = new int[2][2];
-            ArrayList<ArrayList<int[]>> list = new ArrayList<>();
+            int[][] info = new int[2][M];   // 사용자의 이동정보를 담을 배열
+            int[][] BC = new int[A][4];     // BC의 정보를 담을 배열
+            int[][] user = new int[2][2];   // 사용자의 현재 위치를 담을 배열
+            ArrayList<ArrayList<int[]>> list = new ArrayList<>();   // BC별로 범위 정보를 담을 2차원 리스트
             for(int i=0; i<A; i++) {
-                list.add(new ArrayList<>());
+                list.add(new ArrayList<>());    // BC의 개수만큼 리스트 내에 리스트 초기화
             }
+            // 사용자A의 위치
             user[0][0] = 1;
             user[0][1] = 1;
+            // 사용자B의 위치
             user[1][0] = 10;
             user[1][1] = 10;
 
@@ -50,25 +53,25 @@ public class P038_SWEA5644_무선충전 {
                 }
             }
 
-            for (int k=0; k<A; k++) {
+            for (int k=0; k<A; k++) {   // BC의 개수만큼 반복
                 int x = BC[k][0];
                 int y = BC[k][1];
                 int c = BC[k][2];
-                int P = BC[k][3];
 
-                for(int i=0; i<=c; i++) {
-                    for(int j=0; j<=c-i; j++) {
-                        list.get(k).add(new int[] {x+i, y+j});
-                        list.get(k).add(new int[] {x+i, y-j});
-                        list.get(k).add(new int[] {x-i, y+j});
-                        list.get(k).add(new int[] {x-i, y-j});
+                // 모든 좌표 탐색
+                for(int i=1; i<=10; i++) {
+                    for(int j=1; j<=10; j++) {
+                        if(Math.abs(x - i) + Math.abs(y - j) <= c) {    // BC의 범위 내에 있는 좌표이면
+                            list.get(k).add(new int[] {i, j});  // 리스트에 추가
+                        }
                     }
                 }
             }
 
-            for(int i=-1; i<M; i++) {
+            // 이동정보만큼 이동
+            for(int i=-1; i<M; i++) {   // 처음에는 이동하지 않기 위해 -1부터
                 int max = 0;
-                if(i != -1) {
+                if(i != -1) {   // 처음에는 이동하지 않는다
                     switch (info[0][i]) {
                         case 1:
                             user[0][1]--;
@@ -98,39 +101,39 @@ public class P038_SWEA5644_무선충전 {
                             break;
                     }
                 }
-                boolean[][] cnt = new boolean[2][A];
-                int[] a = {user[0][0], user[0][1]};
-                int[] b = {user[1][0], user[1][1]};
-                for(int j=0; j<A; j++) {
-                    cnt[0][j] = contains(list.get(j), a);
-                    cnt[1][j] = contains(list.get(j), b);
+                boolean[][] cnt = new boolean[2][A];    // 사용자별로 어떤 BC 범위내에 포함되는지 저장할 배열
+                int[] a = {user[0][0], user[0][1]}; // 사용자A의 위치
+                int[] b = {user[1][0], user[1][1]}; // 사용자B의 위치
+                for(int j=0; j<A; j++) {    // 모든 BC 탐색
+                    cnt[0][j] = contains(list.get(j), a);   // 사용자A가 해당 BC 범위에 포함되면 true, 아니면 false
+                    cnt[1][j] = contains(list.get(j), b);   // 사용자B가 해당 BC 범위에 포함되면 true, 아니면 false
                 }
 
-                for(int j=0; j<A; j++) {
-                    for(int k=0; k<A; k++) {
-                        if(cnt[0][j]) {
-                            if(cnt[1][k]) {
-                                if(j == k) {
-                                    max = Math.max(max, BC[j][3]);
-                                } else {
-                                    max = Math.max(max, BC[j][3] + BC[k][3]);
+                for(int j=0; j<A; j++) {    // BC 개수만큼 반복
+                    for(int k=0; k<A; k++) {    // BC 개수만큼 또 반복
+                        if(cnt[0][j]) {     // 사용자A가 어떤 BC 범위내에 있다면
+                            if(cnt[1][k]) { // 사용자B도 어떤 BC 범위 내에 있다면
+                                if(j == k) {    // 둘이 같은 BC 범위 내라면
+                                    max = Math.max(max, BC[j][3]);  // max값과 해당 BC의 성능 비교
+                                } else {    // 사용자B는 다른 BC라면
+                                    max = Math.max(max, BC[j][3] + BC[k][3]);   // 두 BC의 성능합과 max 비교
                                 }
-                            } else {
-                                max= Math.max(max, BC[j][3]);
+                            } else {    // 사용자B가 어떤 BC에도 속하지 않다면
+                                max= Math.max(max, BC[j][3]);   // 사용자A가 포함된 BC의 성능만 max와 비교
                             }
-                        } else {
-                            if(cnt[1][k]) {
-                                max = Math.max(max, BC[k][3]);
+                        } else {    // 사용자A가 어떤 BC 범위 내에도 없다면
+                            if(cnt[1][k]) { // 사용자B가 어떤 BC 범위 내에 있다면
+                                max = Math.max(max, BC[k][3]);  // 사용자B가 포함된 BC의 성능만 max와 비교
                             }
+                            // 둘다 어떤 BC 범위에도 없다면 넘어간다
                         }
                     }
                 }
 
-                result += max;
+                result += max;  // max값을 최종 결과에 더한다
             }
 
-
-            sb.append("#").append(tc).append(" ").append(result).append("\n");
+            sb.append("#").append(tc).append(" ").append(result).append("\n");  // 결과 출력
         }
 
         System.out.println(sb);
--- src/박윤환/P040_BJ15683_감시.java
+++ src/박윤환/P040_BJ15683_감시.java
@@ -2,15 +2,193 @@ package 박윤환;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class CCTV {    // cctv 정보를 담을 객체
+    int n, i, j;
+    public CCTV(int n, int i, int j) {
+        this.n = n;     // cctv 종류
+        this.i = i;     // cctv의 행 위치
+        this.j = j;     // cctv의 열 위치
+    }
+}
 
 public class P040_BJ15683_감시 {
+    static final int[] dx = {0, 1, 0, -1};  // 상, 우, 하, 좌
+    static final int[] dy = {-1, 0, 1, 0};  // 상, 우, 하, 좌
+
+    static int N, M, size, min;
+    static int[] dir;
+    static int[][] office;
+    static ArrayList<CCTV> list;
+
+    // cctv 개수에 맞게 돌릴 방향을 정할 중복순열
+    static void perm(int cnt) {
+        // 방향이 정해지면
+        if(cnt == size) {
+            int[][] copy = new int[N][M];   // 원본 사무실배치를 저장할 복사본 배열
+            int count = 0;  // 사각지대를 카운트할 변수
+            for(int i=0; i<N; i++) {
+                copy[i] = Arrays.copyOf(office[i], M);  // 원본 복사
+            }
+            for(int i=0; i<size; i++) {     // list에 있는 cctv만큼 반복
+                int r = list.get(i).i;  // 현재 cctv의 행 위치
+                int c = list.get(i).j;  // 현재 cctv의 열 위치
+                switch(list.get(i).n) { // cctv 종류에 따라
+                    // 1번 종류일 경우
+                    case 1:
+                        // 해당 방향을 계속 감지
+                        while(true) {
+                            r = r + dx[dir[i]];
+                            c = c + dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;   // 끝에 도달하거나
+                            if(copy[r][c] == 6) break;  // 벽을 만나면 종료
+                            copy[r][c] = -1;    // cctv 감지 범위는 -1로 저장
+                        }
+                        break;
+                    // 2번 종류일 경우
+                    case 2:
+                        // 해당 방향을 감지
+                        while(true) {
+                            r = r + dx[dir[i]];
+                            c = c + dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 반대방향을 감지
+                        while(true) {
+                            r = r - dx[dir[i]];
+                            c = c - dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        break;
+                    // 3번 종류일 경우
+                    case 3:
+                        // 해당 방향을 감지
+                        while(true) {
+                            r = r + dx[dir[i]];
+                            c = c + dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 오른쪽으로 90도 꺾어서 감지
+                        while(true) {
+                            r = r + dx[(dir[i]+1)%4];
+                            c = c + dy[(dir[i]+1)%4];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        break;
+                    // 4번 종류일 경우
+                    case 4:
+                        // 해당 방향으로 감지
+                        while(true) {
+                            r = r + dx[dir[i]];
+                            c = c + dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 오른쪽으로 90도 꺾어서 감지
+                        while(true) {
+                            r = r + dx[(dir[i]+1)%4];
+                            c = c + dy[(dir[i]+1)%4];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 왼쪽으로 90도 꺾어서 감지
+                        while(true) {
+                            r = r - dx[(dir[i]+1)%4];
+                            c = c - dy[(dir[i]+1)%4];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        break;
+                    case 5:
+                        // 해당 방향으로 감지
+                        while(true) {
+                            r = r + dx[dir[i]];
+                            c = c + dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 반대 방향으로 감지
+                        while(true) {
+                            r = r - dx[dir[i]];
+                            c = c - dy[dir[i]];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 오른쪽으로 90도 꺾어서 감지
+                        while(true) {
+                            r = r + dx[(dir[i]+1)%4];
+                            c = c + dy[(dir[i]+1)%4];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        r = list.get(i).i;
+                        c = list.get(i).j;
+                        // 왼쪽으로 90도 꺾어서 감지
+                        while(true) {
+                            r = r - dx[(dir[i]+1)%4];
+                            c = c - dy[(dir[i]+1)%4];
+                            if(r<0 || r>=N || c<0 || c>=M) break;
+                            if(copy[r][c] == 6) break;
+                            copy[r][c] = -1;
+                        }
+                        break;
+                }
+            }
+            // 감지가 끝난 배열 탐색
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<M; j++) {
+                    if(copy[i][j] == 0) {   // 사각지대일 경우
+                        count++;    // 카운트 올림
+                    }
+                }
+            }
+            min = Math.min(min, count); // 최소값일 경우 갱신
+
+            return;
+        }
+
+        for(int i=0; i<4; i++) {
+            dir[cnt] = i;
+            perm(cnt+1);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] st = br.readLine().split(" ");
-        int N = Integer.parseInt(st[0]);
-        int M = Integer.parseInt(st[1]);
-        int[][] office = new int[N][M];
+        N = Integer.parseInt(st[0]);
+        M = Integer.parseInt(st[1]);
+        office = new int[N][M];     // 사무실 배치 배열
+        list = new ArrayList<>();   // cctv 정보를 저장할 리스트
 
         for(int i=0; i<N; i++) {
             st = br.readLine().split(" ");
@@ -19,6 +197,22 @@ public class P040_BJ15683_감시 {
             }
         }
 
+        // 사무실 탐색
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                // 빈칸이거나 벽이 아닐 경우 해당 CCTV를 리스트에 추가
+                if(office[i][j] != 0 && office[i][j] != 6) {
+                    list.add(new CCTV(office[i][j], i, j));
+                }
+            }
+        }
+        size = list.size();
+        dir = new int[size];
+        min = (int)1e9;
+
+        perm(0);
+
+        System.out.println(min);
 
 
     }
