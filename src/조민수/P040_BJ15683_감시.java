8370c3fd800e57c60eed3e71246b2e7231a30134
minsu <whalstn1020@gmail.com>
2022-08-18 00:02:05 +0900

    [2022-08-18 조민수] P041 코드수정

--- src/조민수/P040_BJ15683_감시.java
+++ src/조민수/P040_BJ15683_감시.java
@@ -1,17 +1,39 @@
 package 조민수;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class P040_BJ15683_감시 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		//4중포문 돌려서
 		//각각의 경우의 최댓값
+		  String[] st = br.readLine().split(" ");
+	        int N = Integer.parseInt(st[0]);
+	        int M = Integer.parseInt(st[1]);
+	        int[][] office = new int[N][M];
+	        int []IsCctv=new int [6]; //cctv 1~5의 갯수 저장
+	        ArrayList<Integer[][]> CCTV = new ArrayList<Integer[][]>();
+	        for(int i=0; i<N; i++) {
+	            st = br.readLine().split(" ");
+	            for(int j=0; j<M; j++) {
+	            	
+	            	int num=Integer.parseInt(st[j]);
+	                office[i][j] = num;
+	                if(num>=1&&num<=5) {//cctv 유무 판단
+	                	cctv[num]++;
+	                }
+	            }
 		
+	        }
+	        //4중포문
+	        for(int q=1;q<=4;q++)
+	        {
+	        	
+	        }
 	}
-
 }
--- src/조민수/P041_BJ1992_쿼드트리.java
+++ src/조민수/P041_BJ1992_쿼드트리.java
@@ -1,51 +1,76 @@
 package 조민수;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class P041_BJ1992_쿼드트리 {
 
-	
-	static int[][]arr;
-	static int N;
-	static StringBuilder sb;
-	
-	private void dnq(int N,int x, int y) {
-		if(N==1) {
-			return;
-		}
-		//1사분면
-		sb.append("(");
-		dnq(N/2,x,y);
-		sb.append(")");
-		//2사분면
-		sb.append("(");
-		dnq(N/2,x-N/2,y);
-		sb.append(")");
-		//3사분면
-		sb.append("(");
-		dnq(N/2,x,y-N/2);
-		sb.append(")");
-		//4사분면
-		sb.append("(");
-		dnq(N/2,x-N/2,y-N/2);
-		sb.append(")");
-	}
-	
-	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		sb = new StringBuilder();
-		N = Integer.parseInt(br.readLine());
-		arr= new int[N][N];
-		for(int i=0;i<N;i++) {
-			String[] st=br.readLine().split("");
-			for(int j=0;j<N;j++) {
-				arr[i][j]=Integer.parseInt(st[j]);
-			}
-		}
-		System.out.println(sb);
-	}
+    static int N;
+    static int[][] arr;
+    static StringBuilder sb;
 
+    static void dnq(int n, int r, int c) {
+        // 사이즈 1이면
+        if(n == 1) {
+            if(arr[r][c] == 0) {    
+                sb.append(0);   
+            } else if(arr[r][c] == 1) {
+                sb.append(1);  
+            }
+            return;
+        }
+
+        boolean same = true;
+        // 현재 크기내 원소가 
+        for(int i=r; i<r+n; i++) {
+            for(int j=c+1; j<c+n; j++) {//내값과 내 앞값 비교를 위해 시작을 +1로
+            	if(arr[i][j]!=arr[i][j-1]) {//내값과 내 앞값 비교
+            		same=false;
+            		break;//같지않으면 바로 탈출
+            	}
+            }
+            if(!same) {
+            	break;//같지않으면 바로 탈출
+            }
+        }
+        if(same) {  // 값이 다 같다
+            sb.append(arr[r][c]);//아무값이나 하나 넣어주면 됨 편의상 제일 앞값
+        } 
+        else {    // 다르면 4등분 분할해서 연산반복
+            sb.append("(");
+            // 1사분면
+            dnq(n / 2, r, c);
+
+            // 2사분면
+            dnq(n / 2, r, c + n / 2);
+
+            // 3사분면
+            dnq(n / 2, r + n / 2, c);
+
+            // 4사분면
+            dnq(n / 2, r + n / 2, c + n / 2);
+            sb.append(")");
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for(int i=0; i<N; i++) {
+            String[] st = br.readLine().split("");
+            for(int j=0; j<N; j++) {
+                arr[i][j] = Integer.parseInt(st[j]);
+            }
+        }
+
+        dnq(N, 0, 0);
+
+        System.out.println(sb);
+
+    }
 }
