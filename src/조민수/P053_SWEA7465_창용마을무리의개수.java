9a6b0a209e3b3a0f0f660a72ec1e65b812fe60a9
minsu <whalstn1020@gmail.com>
2022-08-24 23:12:22 +0900

    [2022-08-24조민수] P053 커밋 누락

--- src/조민수/P053_SWEA7465_창용마을무리의개수.java
+++ src/조민수/P053_SWEA7465_창용마을무리의개수.java
@@ -11,33 +11,38 @@ public class P053_SWEA7465_창용마을무리의개수 {
 		if(parent[a]==a) {
 			return a;
 		}
-		return parent[a]=find(parent,parent[a]);
+		return find(parent,parent[a]);
 	}
 	
 	
-	static boolean union(int[] parent, int a, int b) {
+	static void union(int[] parent, int a, int b) {
 		int parentA= find(parent,a);
 		int parentB= find(parent,b);
 		
-		if(parentA==parentB) {
-			return false;
+		if(parentA>parentB) {
+			parent[parentA]=parentB;
 		}
 		else {
 			parent[parentB]=parentA;
-			return true;
 		}
+			
+
 	}
 	
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T=Integer.parseInt(br.readLine());
-		HashSet <Integer>  set= new HashSet<>();
+		StringBuilder sb= new StringBuilder();
+		
 		for(int tc=1;tc<=T;tc++) {
+			HashSet <Integer>  set= new HashSet<>();
 			String[] st=br.readLine().split(" ");
 			int N=Integer.parseInt(st[0]);
 			int M=Integer.parseInt(st[1]);
-			int []parent =new int[N];
+			int []parent =new int[N+1];
+			
+			
 			for(int i=1;i<=N;i++) {
 				parent[i]=i;
 			}
@@ -50,14 +55,14 @@ public class P053_SWEA7465_창용마을무리의개수 {
 			for(int[] e:edge) {
 				int a=e[0];
 				int b=e[1];
-				if(union(parent,a,b)) {
-					
-				}
+				union(parent,a,b);
 			}
 			for(int i=1;i<=N;i++) {
-				//set.add();
+				set.add(find(parent,i));
 			}
+			 sb.append("#").append(tc).append(" ").append(set.size()).append("\n");  // HashSet의 크기가 집단의 개수
 		}
+		System.out.println(sb);
 	}
 
 }
