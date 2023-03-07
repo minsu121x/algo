fee154f78a7d562cfdc7199037cdad17d311df89
minsu121x <whalstn1020@hanmail.netgit config --global core.precomposeunicode true\c\cexitgit config --global user.name minsu121x>
2023-03-07 23:46:22 +0900

    [2023-03-07 조민수] P102 중간커밋

---
+++ src/조민수/P102_BJ16922_로마숫자만들기.java
@@ -0,0 +1,13 @@
+package 조민수;
+import java.io.*;
+public class P102_BJ16922_로마숫자만들기 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        int N=Integer.parseInt(br.readLine());
+        //로마숫자 4개(I , V , X , L)로 N자리 만들 수 있는 숫자 -> 4개중 N개 중복조합
+    }
+
+
+
+}
